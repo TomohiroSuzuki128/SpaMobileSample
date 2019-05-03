84083a34744742707450d4035bc4579fbf716b87
Tomohiro Suzuki <suzuchan@muf.biglobe.ne.jp>
2019-05-03 16:23:06 +0900

    wip creating iOS UI

---
+++ JZipSearch/iOS/Views/UIPaddingTextField.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace JZipSearch.iOS
+{
+    public class UIPaddingTextField : UITextField
+    {
+        CGPoint padding = new CGPoint(6.0, 0.0);
+
+        public override CGRect TextRect(CGRect forBounds)
+        {
+            // 通常
+            return forBounds.Inset(padding.X, padding.Y);
+        }
+
+        public override CGRect EditingRect(CGRect forBounds)
+        {
+            // 入力中
+            return forBounds.Inset(padding.X, padding.Y);
+        }
+
+        public override CGRect PlaceholderRect(CGRect forBounds)
+        {
+            // プレースホルダー
+            return forBounds.Inset(padding.X, padding.Y);
+        }
+
+    }
+}
