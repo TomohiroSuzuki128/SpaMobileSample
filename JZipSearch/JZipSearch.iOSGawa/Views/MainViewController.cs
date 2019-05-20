c0758a99fb09a7d3ed21ebd91699d9d934935d02
Tomohiro Suzuki <suzuchan@muf.biglobe.ne.jp>
2019-05-20 17:16:06 +0900

    iOS ガワとSPAを微調整

--- JZipSearch/JZipSearch.iOSGawa/Views/MainViewController.cs
+++ JZipSearch/JZipSearch.iOSGawa/Views/MainViewController.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using CoreGraphics;
 using Foundation;
-using JZipCodeSearchClient;
-using JZipSearch.Core;
 using UIKit;
 
-namespace JZipSearch.iOSAddress.Views
+namespace JZipSearch.iOSGawa.Views
 {
     public partial class MainViewController : UIViewController
     {
@@ -17,46 +15,9 @@ namespace JZipSearch.iOSAddress.Views
             base.ViewDidLoad();
 
             InitializeUI();
-            searchFromZipcodeButton.AddTarget(this, new ObjCRuntime.Selector("zipCodeEvent:"), UIControlEvent.TouchUpInside);
-        }
 
-        [Export("zipCodeEvent:")]
-        async void ZipCodeEvent(NSObject sender)
-        {
-            var zipCode = zipcodeText.Text.Replace("-", "");
-
-            if (string.IsNullOrWhiteSpace(zipCode))
-            {
-                PresentAlert("郵便番号を入力してください。");
-                return;
-            }
-
-            if (zipCode?.Any(c => !char.IsNumber(c)) == true)
-            {
-                PresentAlert("郵便番号は数字のみ入力してください。");
-                return;
-            }
-
-            var addressList = await JZipSearchClient.ZipToAddress(zipCode);
-
-            if (addressList?.Any() == false)
-                PresentAlert("該当する情報が見つかりません.");
-
-            prefectureText.Text = addressList.FirstOrDefault().Prefecture;
-            cityText.Text = addressList.FirstOrDefault().City;
-            addressText.Text = addressList.FirstOrDefault().Machi;
-
-            prefecturePicker.Select(
-            nint.Parse(Prefectures.All().FirstOrDefault(x => x.Name == addressList.FirstOrDefault().Prefecture).Code) - 1,
-            0, false);
-
-        }
-
-        void PresentAlert(string message)
-        {
-            var alertController = UIAlertController.Create(string.Empty, message, UIAlertControllerStyle.Alert);
-            alertController.AddAction(UIAlertAction.Create("閉じる", UIAlertActionStyle.Cancel, null));
-            PresentViewController(alertController, true, null);
+            var request = NSUrlRequest.FromUrl(NSUrl.FromString("https://decode19sap.azurewebsites.net/index.html"));
+            webView.LoadRequest(request);
         }
 
     }
